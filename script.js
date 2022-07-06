0016cf9e68475229cad7a8a847011d0eb1a038bc
my e. lovell <48379556+myelovell@users.noreply.github.com>
2022-07-06 11:59:46 +0200

    animations work 100%, not pretty tho

--- script.js
+++ script.js
@@ -12,10 +12,21 @@ const gridSpots ={
     '8': 'bottom-right',
     '9': 'N/A'
 };
+
+
     
 const params = new URLSearchParams(window.location.search)
 const duration = params.get("duration")
 const template = params.get("template")
+
+const imageAnimation = [
+    `kenburns-top ${duration}s linear both reverse 0s, fade-out 5s ease-in forwards ${duration- 2}s`,
+    `kenburns-right ${duration}s linear both reverse 0s, fade-out 2s ease-in forwards ${duration - 2}s`,
+    `kenburns-bottom ${duration}s linear both 0s, fade-out 2s ease-in forwards ${duration- 2}s`,
+    `kenburns-left ${duration}s linear both 0s, fade-out 2s ease-in forwards ${duration- 2}s`,
+]
+
+const textAnimation = `focus-in-contract 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards 0s, text-blur-out 2s ease-in forwards ${duration- 2}s`
 
 function getAll() {
     fetch("/mockData.json")
@@ -26,8 +37,6 @@ function getAll() {
 
 function generate(data) {
     for (let i = 0; i < data.length; i++) {
-      //console.log(data[i])
-  
       var img = document.createElement("img");
       img.src = `images/${data[i].url}`;
       img.classList = `img${i} img`
@@ -38,9 +47,9 @@ function generate(data) {
       var title = document.createElement("h1");
       title.innerHTML = data[i].title
       title.classList = `title${i} title slot${data[i].slot}`
+
       var titles = document.querySelector(".titles");
       titles.appendChild(title)
-
     }
     generateSlideshow(data)
 }
@@ -51,12 +60,15 @@ function delay(n){
     });
 }
 
+
+
 async function generateSlideshow(data) {
     for (let x = 0; x < data.length; x++) {
         let rest = document.querySelector(`.img${x}`)
         rest.style.opacity = 0;
         let r = document.querySelector(`.title${x}`)
         r.style.opacity = 0;
+        console.log(r)
     }
     let playing = [data[0]]
 
@@ -66,53 +78,77 @@ async function generateSlideshow(data) {
             playing.push(data[i + 1])
             let activeImg = document.querySelector(`.img0`)
             activeImg.style.opacity = 100;
+            activeImg.style.zIndex = 15;
+            let activeImg2 = document.querySelector(`.img${i + 1}`)
+            activeImg2.style.opacity = 100;
             let activeTitle = document.querySelector(`.title0`)
             activeTitle.style.opacity = 100;
-            handleTemplate("first index", i)
+            handleTemplate("first index", i, activeImg, activeTitle)
 
         } else if (i == data.length - 1) {
             playing.shift()
             playing.push(data[0])
-            let rest = document.querySelector(`.img${i}`)
-            rest.style.opacity = 100;
+            let activeImg = document.querySelector(`.img${i}`)
+            activeImg.style.opacity = 100;
+            activeImg.style.zIndex = 15;
+            let activeImg2 = document.querySelector(`.img${0}`)
+            activeImg2.style.opacity = 100;
             let activeTitle = document.querySelector(`.title${i}`)
             activeTitle.style.opacity = 100;
-            handleTemplate("last index", i)
+            handleTemplate("last index", i, activeImg, activeTitle)
 
         } else {
             playing.shift()
             playing.push(data[i + 1])
             let activeImg = document.querySelector(`.img${i}`)
             activeImg.style.opacity = 100;
+            activeImg.style.zIndex = 15;
+            let activeImg2 = document.querySelector(`.img${i + 1}`)
+            activeImg2.style.opacity = 100;
             let activeTitle = document.querySelector(`.title${i}`)
             activeTitle.style.opacity = 100;
-            handleTemplate("index", i)
+            handleTemplate("index", i, activeImg, activeTitle)
         }
        
-
+        
         console.log(playing)
         await delay(duration)
         let r = document.querySelector(`.title${i}`)
         r.style.opacity = 0;
+        r.style.animation = ''
 
     }
     generateSlideshow(data)
 }
 
-function handleTemplate(x, i) {
-    console.log(`handeling template${template}`)
-    if (x == "first index") {
-        let activeTitle = document.querySelector(`.title0`)
-        activeTitle.classList.add(`textAnimation`)
-    } else if (x == "index") {
-        let activeTitle = document.querySelector(`.title${i}`)
-        activeTitle.classList.add(`textAnimation`)
-    } else if(x == "last index") {
-        let activeTitle = document.querySelector(`.title${i}`)
-        activeTitle.classList.add(`textAnimation`)
-    } else {
-        console.log("error")
+let style = 0
+function handleTemplate(x, i, activeImg, activeTitle) {
+
+    if (style == 4) {
+        style = 0;
     }
+    console.log(`handeling template${template}`)
+    console.log(style)
+    if (template == 0) {
+        if (x == "first index") {
+            //let activeTitle = document.querySelector(`.title0`)
+            activeTitle.style.animation = textAnimation;
+            activeImg.style.animation = imageAnimation[style]
+        } else if (x == "index") {
+            //let activeTitle = document.querySelector(`.title${i}`)
+            activeTitle.style.animation = textAnimation;
+            activeImg.style.animation = imageAnimation[style]
+        } else if(x == "last index") {
+            //let activeTitle = document.querySelector(`.title${i}`)
+            activeTitle.style.animation = textAnimation;
+            activeImg.style.animation = imageAnimation[style]
+        } else {
+            console.log("error")
+        }
+        style += 1
+        console.log(style)
+    }
+
     
 }
 
